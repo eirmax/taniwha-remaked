fdf5afc805f86a3c8a0a986f54dd3b4811d3b55a
eirmax <eirmax8@gmail.com>
2025-09-07 17:08:40 +0700

    Add EventBusSubscribe

--- neoforge/src/main/java/party/lemons/taniwha/neoforge/TaniwhaNeoForge.java
+++ neoforge/src/main/java/party/lemons/taniwha/neoforge/TaniwhaNeoForge.java
@@ -5,18 +5,17 @@ import dev.architectury.utils.Env;
 import dev.architectury.utils.EnvExecutor;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import party.lemons.taniwha.TConstants;
 import party.lemons.taniwha.Taniwha;
 
 @Mod(TConstants.MOD_ID)
-@EventBusSubscriber(modid = TConstants.MOD_ID, value = Dist.CLIENT)
 public class TaniwhaNeoForge {
 
-    public TaniwhaNeoForge(IEventBus modEventBus) {
+    public TaniwhaNeoForge() {
         Taniwha.init();
-        modEventBus.addListener(TForgeEvents::onPlaceEvent);
 
         EnvExecutor.runInEnv(Env.CLIENT, ()-> TForgeClientEvents::initClient);
     }
